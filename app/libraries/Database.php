5084baf292f8cad7a7a564967b8fad213cac4064
jctiru <jonathanchristiantiru@gmail.com>
2018-01-23 16:57:34 +0800

    Added framework essentials

---
+++ app/libraries/Database.php
@@ -0,0 +1,48 @@
+<?php 
+	/*
+	 * PDO Database Class
+	 * Connect to database
+	 * Create prepared statements
+	 * Bind values
+	 * Return rows and results
+	 */
+	class Database {
+		private $host = DB_HOST;
+		private $user = DB_USER;
+		private $pass = DB_PASS;
+		private $dbname = DB_NAME;
+
+		private $dbh;
+		private $stmt;
+		private $error;
+
+		public function __construct(){
+			// Set DSN
+			$dsn = 'mysql: host='. $this->host .';dbname=' . $this->dbname;
+			$options = array(
+				PDO::ATTR_PERSISTENT => true,
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+				PDO::ATTR_EMULATE_PREPARES => false
+			);
+
+			// Create PDO instance
+			try{
+				$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+			} catch(PDOException $e){
+				$this->error = $e->getMessage();
+				echo $this->error;
+			}
+		}
+
+		// Prepare statement with query
+		public function query($sql){
+			$this->stmt = $this->dbh->prepare($sql);
+		}
+
+		// Bind values
+		public function bind($param, $value, $type = null){
+			
+		}
+	}
+ ?>

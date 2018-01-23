949daab574f57b5420cad39d50ad15c6506aea09
jctiru <jonathanchristiantiru@gmail.com>
2018-01-23 18:30:25 +0800

    Finished mvc framework

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,13 @@
 <?php 
 	class Pages extends Controller {
 		public function __construct(){
-			$this->postModel = $this->model('Post');
+
 		}
 
 		public function index(){
-			$data = ['title'=> 'Welcome'];
+			$data = [
+				'title'=> 'Welcome'
+			];
 			$this->view('pages/index', $data);
 		}
 
